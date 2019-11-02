49666d79b652b33468faa30da8c2657df3315aa3
kirill <stealer-mail@mail.ru>
2019-11-02 14:51:52 +0500

    add registration

--- maineco/views.py
+++ maineco/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from maineco.models import Event
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required, user_passes_test
@@ -13,19 +15,28 @@ from django.shortcuts import get_object_or_404
 from django.contrib import messages
 from django.http import JsonResponse
 from groups.models import Group
-
+from django.contrib.auth.forms import UserCreationForm
+from maineco.forms import SignUpForm
+from users.views import show_forms_errors
+from django.views.decorators.http import require_POST, require_GET
+from django.contrib.sites.shortcuts import get_current_site
 
 def gen_hash():
     hash_user = str(uuid.uuid4())
     return hash_user.replace("-", "")
 
+
 def main(request):
+
     if request.user.is_authenticated:
-        return render(request,'index.html')
+        return render(request, 'index.html')
     else:
         return redirect('about/')
 
+
 def about(request):
+    print(request.get_host())
+    print(request.scheme)
     groups = Group.objects.all()
     return render(request, 'about.html', {'groups': groups, 'user': request.user})
 
@@ -46,43 +57,40 @@ def loginuser(request):
 
 
 def sendemail(email, hash_code, request):
-    baseurl = request.scheme + ':' + request.META['HTTP_HOST']
+    baseurl = request.scheme + '://' + request.get_host()
     message = render_to_string('mail/message.html', {'hash': hash_code, "baseurl": baseurl})
     send_mail(subject='Добро пожаловать !', message=" ",
-              from_email=settings.EMAIL_HOST_USER, recipient_list=[email, ], fail_silently=False, html_message=message)
+              from_email='clear-ufa@info.ru', recipient_list=[email, ], fail_silently=False, html_message=message)
 
 
 def validate_hash(request, hash_code):
     user = get_object_or_404(User, hash_code=hash_code)
-    user.is_confirmed = True
-    user.hash_code = 0
-    user.save()
-    return redirect('/')
+    if not user.is_confirmed:
+        user.is_confirmed = True
+        user.hash_code = 0
+        user.save()
+    return redirect('/personal-data/')
 
 
 def signup(request):
-    if request.method == "POST":
-        username = request.POST['username']
-        password_new = request.POST['password_new']
-        password_confirmed = request.POST['password_confirmed']
-        if password_new == password_confirmed:
-            try:
-                User.objects.get(email=username)
-                return render(request, 'registration.html', {"error": "Аккаунт с таким адресом уже существует"})
-            except:
-                user = User(email=username)
-                user.hash_code = gen_hash()
-                user.save()
-                user.set_password(password_new)
-                user.save()
-                sendemail(user.email, user.hash_code, request)
-                user = authenticate(request, username=username, password=password_new)
-                if user is not None:
-                    login(request, user)
-                    return redirect('/')
-        else:
-            return render(request, 'registration.html', {"error": "Пароли не совпадают"})
-    return render(request, 'registration.html')
+    form = SignUpForm()
+    return render(request, 'registration.html', {'form': form})
+
+
+@require_POST
+def signup_user(request):
+    form = SignUpForm(request.POST)
+    if form.is_valid():
+        form.save()
+        user = authenticate(request, username=form.cleaned_data['email'], password=form.cleaned_data['password1'])
+        user.hash_code = gen_hash()
+        user.save()
+        sendemail(user.email, user.hash_code, request)
+        login(request, user)
+        return redirect('/personal-data/')
+    else:
+        show_forms_errors(request, form)
+        return render(request, 'registration.html', {'form': form})
 
 
 def logout(request):
@@ -92,7 +100,7 @@ def logout(request):
 
 def join_event(request):
     status = 202
-    user =request.user
+    user = request.user
     if request.is_ajax():
         event = Event.objects.get(id=request.POST['id_event'])
         if user.is_authenticated:
@@ -112,4 +120,6 @@ def join_event(request):
             message = 'Авторизуйтесь или зарегистрируйтесь для того чтобы учавстовать в мероприятиях!'
             tag = 'error'
             status = 401
-    return JsonResponse({'message': message, "tag": tag, "member_count": event.get_count_members(),"id_event":event.id}, safe=False,status=status)
+    return JsonResponse(
+        {'message': message, "tag": tag, "member_count": event.get_count_members(), "id_event": event.id}, safe=False,
+        status=status)
